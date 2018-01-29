3c1346419bad35c19ae1b143ba001717ac76ae76
Rostik rast <roostik21@gmail.com>
2018-01-29 17:48:18 +0200

    added manage controller nav menu moved to file _includes.nav.menu

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'manage', 'middleware' => 'role:superadministrator'], function () {
+    Route::get('dashboard', 'ManageController@dashboard')->name('manage.dashboard');
+});
