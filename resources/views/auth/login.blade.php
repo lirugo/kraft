f4f5f73a972995ddde42307edb372a7f74017eab
Maksym <dolgov.name@gmail.com>
2018-02-05 12:18:44 +0200

    Все работы по css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+            <div class="panel-login panel-default">
+                <div class="panel-heading-login">To login the system<br>you must authorization</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
@@ -50,7 +50,7 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="botton botton-primary">
                                     Login
                                 </button>
 
