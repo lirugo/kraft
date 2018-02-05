38f88c48428764fb5c6b5558036ba354896262f5
Rostik rast <roostik21@gmail.com>
2018-02-05 16:18:57 +0200

    Исправленна ошибка в позиционировании middleware для middleware languageSwitcher

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,8 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \App\Http\Middleware\LanguageSwitcher::class,
     ];
 
     /**
@@ -30,6 +28,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\LanguageSwitcher::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
