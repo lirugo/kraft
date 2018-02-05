5b84f70c6c18399f78594c9600e86f7e50822f15
Rostik rast <roostik21@gmail.com>
2018-02-05 16:50:59 +0200

    Добавленна страница 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="error-page">
+                            <div class="error-content">
+                                <h3><i class="fa fa-warning text-yellow"></i> 403 Oops! Access denied.</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
