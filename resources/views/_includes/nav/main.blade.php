74a965ccade35ee24b70bc0d80a35531984d49fc
Maksym <dolgov.name@gmail.com>
2018-02-06 00:42:07 +0200

    Исправлен: app.css style.css main.blade.php login.blade.php
    Поправлены все формы и кнопки.
    
    Также добавлены некоторые стили: группа стилей Margin-left

--- resources/views/_includes/nav/main.blade.php
+++ resources/views/_includes/nav/main.blade.php
@@ -29,9 +29,9 @@
                     <li><a href="{{ url('/login') }}">{{ trans('app.login') }}</a></li>
                     <li><a href="{{ url('/register') }}">{{ trans('app.registration') }}</a></li>
                     <li>
-                        <form action="/language" method="post" class="m-t-30">
+                        <form action="/language" method="post" class="m-t-30 m-l-20">
                             <input type=image src="/img/icon/flags/ru.svg" name="locale" value="ru" class="flag" alt="Russian">
-                            <input type=image src="/img/icon/flags/en.svg" name="locale" value="en" class="flag" alt="English">
+                            <input type=image src="/img/icon/flags/en.svg" name="locale" value="en" class="flag m-l-10" alt="English">
                             {{ csrf_field() }}
                         </form>
                     </li>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="all-form" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,7 +28,7 @@
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="all-form" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -42,7 +42,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> <span class="panel-login-rem">Remember Me</span>
                                     </label>
                                 </div>
                             </div>
@@ -50,7 +50,7 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="botton botton-primary">
+                                <button type="submit" class="botton botton-primary botton-default">
                                     Login
                                 </button>
 
