e5ab9f6a4096fcacfc797b42b4c25360a6f8ae1f
Maksym <dolgov.name@gmail.com>
2018-02-10 20:06:23 +0200

    Укр язык+перевод.

---
+++ resources/lang/ua/app.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    'language' => 'Мова',
+    'login' => 'Ввійти',
+    'registration' => 'Регістрація',
+
+    //Welcome page
+    'h4welcome' => 'Легкий вибір - Кращий результат',
+    'h1welcometext' => 'Системи підвісних стель європейської якості',
+    //End Welcome page
+
+    //Footer
+    'footertext' => 'Всі права захищені. При передруці матеріалів посилання на сайт є обов\'язковим. SmalLTeam',
+    //EndFooter
+
+    //Profile
+    'male' => 'Чоловіча',
+    'femele' => 'Жіноча',
+    //EndProfile
+
+    //Menu
+    'main' => 'Головна',
+    'manage' => 'Панель управління',
+    'objects' => 'Об\'єкти',
+    //EndMenu
+
+    //Manage
+    'managers' => 'Менеджери',
+    'distributors' => 'Дистриб\'ютори',
+    'statistics' => 'Статистика',
+    'profilecompany' => 'Профіль компанії',
+    'userscompany' => 'Користувачі компанії',
+    'profileuser' => 'Профіль користувача',
+    'calculator' => 'Калькулятор',
+    'invoice' => 'Рахунки',
+    'history' => 'Історія',
+    //EndManage
+];
---
+++ resources/lang/ua/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль повинен містити не менше 6 символів.',
+    'reset' => 'Ваш пароль скинутий!',
+    'sent' => 'Ми відправили Вам лист з інструкцією по зміні пароля!',
+    'token' => 'Це посилання не дійсне.',
+    'user' => "Користувача з даною адресою не існує.",
+
+];
