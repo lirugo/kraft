573a3870fae5c36a58cf0aed5cc78fc25058dd1e
Maksym <dolgov.name@gmail.com>
2018-02-10 16:26:56 +0200

    Перевод.

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Данная учетная запись отсутствует.',
+    'throttle' => 'Слишком много попыток. Попробуйте через :seconds секунд.',
 
 ];
