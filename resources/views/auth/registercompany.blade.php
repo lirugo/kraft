fe12cb07845bd1ec91e4f16001621962b511355c
Rostik rast <roostik21@gmail.com>
2018-03-01 13:56:04 +0200

    менеджер отображение объектов

--- resources/views/auth/registercompany.blade.php
+++ resources/views/auth/registercompany.blade.php
@@ -110,18 +110,18 @@
                     <section>
                         <div class="col-md-4">
                             {!! Form::label('label', trans('app.profileuser')) !!}
-                            {!! Form::text('surname', null , ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.surname')]) !!}
-                            {!! Form::text('name', null , ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.name')]) !!}
-                            {!! Form::text('patronymic', null , ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.patronymic')]) !!}
-                            {!! Form::text('dateofbirth',null , ['class' => 'form-control m-b-10 required', 'id' => 'datepicker', 'placeholder' => trans('app.date')]) !!}
+                            {!! Form::text('surname', null , ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.surname')." *"]) !!}
+                            {!! Form::text('name', null , ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.name')." *"]) !!}
+                            {!! Form::text('patronymic', null , ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.patronymic')." *"]) !!}
+                            {!! Form::text('dateofbirth',null , ['class' => 'form-control m-b-10 required', 'id' => 'datepicker', 'placeholder' => trans('app.date')." *"]) !!}
                             {!! Form::select('sex', ['X' => 'Man', 'Y' => 'Woman'], null, ['class' => 'form-control m-b-10 required']); !!}
                             {!! Form::text('phone', null , ['class' => 'form-control m-b-10 required phone', 'placeholder' => trans('app.phone')." *"]) !!}
-                            {!! Form::email('email', null,['class' => 'form-control m-b-10 required email', 'placeholder' => trans('app.email')]) !!}
+                            {!! Form::email('email', null,['class' => 'form-control m-b-10 required email', 'placeholder' => trans('app.email')." *"]) !!}
                         </div>
                         <div class="col-md-4">
                             {!! Form::label('label', trans('app.password')) !!}
-                            {!! Form::password('password', ['class' => 'form-control m-b-10 required' , 'placeholder' => trans('app.password')]) !!}
-                            {!! Form::password('password_confirmation', ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.confirmpassword')]) !!}
+                            {!! Form::password('password', ['class' => 'form-control m-b-10 required' , 'placeholder' => trans('app.password')." *"]) !!}
+                            {!! Form::password('password_confirmation', ['class' => 'form-control m-b-10 required', 'placeholder' => trans('app.confirmpassword')." *"]) !!}
 
                             <br>
                             <h5> You must upload your photo. </h5>
--- resources/views/manager/objects.blade.php
+++ resources/views/manager/objects.blade.php
@@ -30,16 +30,30 @@
                     <tbody>
                     @foreach($objects as $object)
                         @if($object->active == true)
-                            <tr>
+                            @php
+                                $reportdiff = Carbon\Carbon::parse($object->reports->last()->dateofreport)->diff(Carbon\Carbon::now());
+                            @endphp
+                            <tr bgcolor="@if($reportdiff->days <= 3 && $reportdiff->invert == 1) #ffff81 @elseif($reportdiff->days > 3 && $reportdiff->invert == 1) white @else #fe8081 @endif">
                                 <th scope="row">{{$object->id}}</th>
                                 <td>{{$object->name}}</td>
                                 <td>{{$object->getregion->regionname_ru}}</td>
                                 <td>{{$object->user->name." ".$object->user->surname}}</td>
                                 <td>{{ Carbon\Carbon::parse($object->created_at)->format('Y-m-d') }}</td>
                                 <td>{{$object->dateofdelivery}}</td>
-                                <td>{{$object->dateofreport}}</td>
                                 <td>
-                                    <a href="/manager/object/show/{{$object->id}}" class="btn btn-danger btn-sm pull-right">Просмотреть</a>
+                                    @php
+                                        if($reportdiff->invert == 0 && $reportdiff->days == 0)
+                                            echo "Отчет нужно сдать сегодня!";
+                                        else  if($reportdiff->invert == 1 && $reportdiff->days == 0)
+                                            echo "Отчет нужно сдать завтра!";
+                                        else  if($reportdiff->invert == 1)
+                                            echo "Дней осталось: ".$reportdiff->days;
+                                        else  if($reportdiff->invert == 0)
+                                            echo "Дней просрочено: ".$reportdiff->days;
+                                    @endphp
+                                </td>
+                                <td>
+                                    <a href="/manager/object/show/{{$object->id}}" class="btn btn-danger btn-sm pull-right">Просмотреть<br></a>
                                 </td>
                             </tr>
                         @endif
