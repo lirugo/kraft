569c7d824ce5b06eaf29a53faf395643eaf8f124
lirugo <roostik21@gmail.com>
2018-04-05 18:37:59 +0300

    тестовое поле для номера телефона

--- resources/views/_includes/footer.blade.php
+++ resources/views/_includes/footer.blade.php
@@ -1,14 +1,14 @@
-    <div class="footer">
-        <div class="footer-text">
-            <p class="text-center">
-                © {{ date('Y')}} {{ trans('app.footertext') }}
-                @if(!Auth::guest())
+<div class="footer">
+    <div class="footer-text">
+        <p class="text-center">
+            © {{ date('Y')}} {{ trans('app.footertext') }}
+            @if(!Auth::guest())
                 @if(Auth::user()->hasRole('distributor') || Auth::user()->hasRole('designer') || Auth::user()->hasRole('worker'))
                     <a id="contact-form" href="#" class="contact-menu pull-right bnam" style="">Написать в Kraft</a>
                 @endif
-                @endif
-            </p>
-                {{--<li><a id="contact-form" href="#" class="contact-menu">Связь с KRAFT</a></li>--}}
-        </div>
-
+            @endif
+        </p>
+        {{--<li><a id="contact-form" href="#" class="contact-menu">Связь с KRAFT</a></li>--}}
     </div>
+
+</div>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,23 +1,42 @@
 @extends('layouts.app')
 @section('stylesheets')
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css">
 
 @endsection
-@section('content')
 
+@section('content')
     <div class="row">
         <div class="col-md-4 col-md-offset-1">
             <div class="form-inline">
-                {!! Form::select('hrp',[
-                    '+38',
-                    '+7',
-                ], null, ['class' => 'form-control']) !!}
+                <select class="icons_select2">
+                    <option value="+38" data-icon="fa-user">+38</option>
+                    <option value="+7" data-icon="fa-dropbox">+7</option>
+                </select>
                 {!! Form::text('phone', null, ['class' => 'form-control', 'placeholder' => 'XXX XX XX']) !!}
             </div>
         </div>
     </div>
 
 @endsection
-@section('scripts')
 
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+    <script>
+        function iformat(icon) {
+            var originalOption = icon.element;
+            return $('<span><img src="/img/icon/flags/ua.png"/> ' + icon.text + '</span>');
+        }
+        $('.icons_select2').select2({
+            width:80,
+            minimumResultsForSearch: -1,
+            templateSelection: iformat,
+            templateResult: iformat,
+            allowHtml: true
+        });
+        $('b[role="presentation"]').hide();
+        $('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');
+    </script>
 @endsection
