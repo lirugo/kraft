c1e00a8be04a61dba4fdbd7a3c3fae3daa5f372d
lirugo <roostik21@gmail.com>
2018-07-05 17:55:54 +0300

    Test send to 1C

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,57 +17,6 @@ use SoapFault;
 class TestController extends Controller
 {
     public function index(){
-        function Connect1C(){
-            if (!function_exists('is_soap_fault')){
-                print 'Не настроен web сервер. Не найден модуль php-soap.';
-                return false;
-            }
-            try {
-                $Клиент1С = new SoapClient('http://10.200.5.44/ERP/hs/ExchengeKalk/',
-                    array('login'          => 'Админ',
-                        'password'       => '3',
-                        'soap_version'   => SOAP_1_2,
-                        'cache_wsdl'     => WSDL_CACHE_NONE, //WSDL_CACHE_MEMORY, //, WSDL_CACHE_NONE, WSDL_CACHE_DISK or WSDL_CACHE_BOTH
-                        'exceptions'     => true,
-                        'trace'          => 1));
-            }catch(SoapFault $e) {
-                trigger_error('Ошибка подключения или внутренняя ошибка сервера. Не удалось связаться с базой 1С.', E_ERROR);
-                var_dump($e);
-            }
-            //echo 'Раз';
-            if (is_soap_fault(Клиент1С)){
-                trigger_error('Ошибка подключения или внутренняя ошибка сервера. Не удалось связаться с базой 1С.', E_ERROR);
-                return false;
-            }
-            return $Клиент1С;
-        }
-
-        function GetData($idc, $txt){
-            if (is_object($idc)){
-
-                try {
-                    $par = array('zapros' => $txt);
-                    //var_dump($par);
-                    $ret1c = $idc->hellobaza($par);
-                } catch (SoapFault $e) {
-                    echo "АЩИБКА!!! </br>";
-                    var_dump($e);
-                }
-            }
-            else{
-                echo 'Не удалося подключиться к 1С';
-                var_dump($idc);
-            }
-            return $ret1c;
-        }
-
-        $idc = Connect1C();
-        $ret1c = GetData($idc, "привет");
-        //var_dump($ret1c);
-        $aa=$ret1c->return;
-        echo "!!$aa!!";
-
-        dd('OK');
         return view('test');
     }
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,33 +4,44 @@
 @endsection
 
 @section('content')
+    <?php
 
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_PORT => "8384",
+        CURLOPT_URL => "http://erp.kraftds.com:8384/ERP/hs/ExchengeKalk",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"text\"\r\n\r\nHere some test text from post \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+        CURLOPT_HTTPHEADER => array(
+            "Cache-Control: no-cache",
+            "Postman-Token: c4883a14-6c87-4d30-a73e-779ba4f06492",
+            "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+        ),
+    ));
+    curl_setopt($curl, CURLOPT_USERPWD, "Admin:3");
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo $response;
+        dd($response);
+    }
+    ?>
 
 @endsection
 
 
 @section('scripts')
-    <script>
-        $(document).ready(function () {
-            var obj = {
-                val1: "this",
-                val2: "that"
-            };
-            obj.val3 = 'these';
-            obj['val4'] = 'those';
 
-            $.ajax({
-                type: "POST",
-                url: "http://10.200.5.44/ERP/hs/ExchengeKalk/",
-                data: {
-                    "_token": "{{ csrf_token() }}",
-                    json: JSON.stringify(obj)
-                },
-                success: function (response) {
-                    //service.php response
-                    console.log(response);
-                }
-            });
-        });
-    </script>
+
 @endsection
