00cc35b554199905df3ded13cf6fbe41800c4ede
lirguo <roostik21@gmail.com>
2018-10-01 11:12:04 +0300

    Fix #6.3

--- resources/views/manager/user.blade.php
+++ resources/views/manager/user.blade.php
@@ -66,32 +66,51 @@
                     <tbody>
                     @foreach($distributors->getcompany->objects as $object)
                         @if($object->active == true)
-                            @php
-                                $reportdiff = Carbon\Carbon::parse($object->reports->last()->dateofreport)->diff(Carbon\Carbon::now());
-                            @endphp
-                            <tr bgcolor="@if($reportdiff->days <= 3 && $reportdiff->invert == 1) #ffff81 @elseif($reportdiff->days > 3 && $reportdiff->invert == 1) white @else #fe8081 @endif">
-                                <th scope="row">{{$object->id}}</th>
-                                <td>{{$object->name}}</td>
-                                <td>{{$object->getregion->regionname_ru}}</td>
-                                <td>{{$object->user->name}}</td>
-                                <td>{{ Carbon\Carbon::parse($object->created_at)->format('Y-m-d') }}</td>
-                                <td>{{$object->dateofdelivery}}</td>
-                                <td>
-                                    @php
-                                        if($reportdiff->invert == 0 && $reportdiff->days == 0)
-                                            echo trans('app.Without report');
-                                        else  if($reportdiff->invert == 1 && $reportdiff->days == 0)
-                                            echo trans('app.Report need send tomorrow');
-                                        else  if($reportdiff->invert == 1)
-                                            echo trans('app.Days left').' '.$reportdiff->days;
-                                        else  if($reportdiff->invert == 0)
-                                            echo trans('app.Days overdue').' '.$reportdiff->days;
-                                    @endphp
-                                </td>
-                                <td>
-                                    <a href="/manager/object/show/{{$object->id}}" class="btn btn-danger btn-sm pull-right">{{trans('app.View')}}<br></a>
-                                </td>
-                            </tr>
+                            @if($object->reports->last())
+                                @php
+                                    $reportdiff = Carbon\Carbon::parse($object->reports->last()->dateofreport)->diff(Carbon\Carbon::now());
+                                @endphp
+                                <tr bgcolor="@if($reportdiff->days <= 3 && $reportdiff->invert == 1) #ffff81 @elseif($reportdiff->days > 3 && $reportdiff->invert == 1) white @else #fe8081 @endif">
+                                    <th scope="row">{{$object->id}}</th>
+                                    <td>{{$object->name}}</td>
+                                    <td>{{$object->getregion->regionname_ru}}</td>
+                                    <td>{{$object->user->name}}</td>
+                                    <td>{{ Carbon\Carbon::parse($object->created_at)->format('Y-m-d') }}</td>
+                                    <td>{{$object->dateofdelivery}}</td>
+                                    <td>
+                                        @php
+                                            if($reportdiff->invert == 0 && $reportdiff->days == 0)
+                                                echo trans('app.Without report');
+                                            else  if($reportdiff->invert == 1 && $reportdiff->days == 0)
+                                                echo trans('app.Report need send tomorrow');
+                                            else  if($reportdiff->invert == 1)
+                                                echo trans('app.Days left').' '.$reportdiff->days;
+                                            else  if($reportdiff->invert == 0)
+                                                echo trans('app.Days overdue').' '.$reportdiff->days;
+                                        @endphp
+                                    </td>
+                                    <td>
+                                        <a href="/manager/object/show/{{$object->id}}" class="btn btn-danger btn-sm pull-right">{{trans('app.View')}}<br></a>
+                                    </td>
+                                </tr>
+                            @else
+                                <tr bgcolor="white">
+                                    <th scope="row">{{$object->id}}</th>
+                                    <td>{{$object->name}}</td>
+                                    <td>{{$object->getregion->regionname_ru}}</td>
+                                    <td>{{$object->user->name}}</td>
+                                    <td>{{ Carbon\Carbon::parse($object->created_at)->format('Y-m-d') }}</td>
+                                    <td>{{$object->dateofdelivery}}</td>
+                                    <td>
+                                        {{trans('app.Without report')}}
+                                    </td>
+                                    <td>{{$object->rmuser->name}}</td>
+                                    <td>{{$object->rmuser->phone}}</td>
+                                    <td>
+                                        <a href="/manager/object/show/{{$object->id}}" class="btn btn-danger btn-sm pull-right">{{trans('app.View')}}<br></a>
+                                    </td>
+                                </tr>
+                            @endif
                         @endif
                     @endforeach
                     </tbody>
