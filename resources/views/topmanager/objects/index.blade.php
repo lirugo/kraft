4fb8ef0af4418833594cda4954c487a225586d48
lirugo <roostik21@gmail.com>
2020-11-04 13:24:02 +0200

    Update index.blade.php

--- resources/views/topmanager/objects/index.blade.php
+++ resources/views/topmanager/objects/index.blade.php
@@ -93,6 +93,7 @@
                                         </td>
                                         <td>{{$object->name}}</td>
                                         <td>{{$object->getregion->regionname_ru}}</td>
+                                        <td>{{$object->user ? $object->user->name : ''}}</td>
                                         <td>{{ Carbon\Carbon::parse($object->created_at)->format('Y-m-d') }}</td>
                                         <td>{{$object->dateofdelivery}}</td>
                                         <td>
